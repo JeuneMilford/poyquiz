49d5f47a6b03291373d9913d71f0ea11caa9678f
Jeune Milford <milford.j@maristcollege.school.nz>
2022-07-07 13:14:28 +1200

    basics

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const StartButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById
+('question-container')
+
+const shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
 console.log('Started')
 startButton.classlist.add('hide')
-
+questionContainerElement.classList.remove('hide')
+setNextQuestion()
 }
 
 function setNextQuestion() {
@@ -15,3 +20,13 @@ function setNextQuestion() {
 function selectAnswer() {
 
 }
+
+const question = [
+  {
+    question:'What is 2 + 2',
+    answers: [
+        { Text:'4', correct: true},
+        {Text:'22', correct:false}
+    ]
+  }
+]

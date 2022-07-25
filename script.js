7f4fb322831b0288aac3e4b215ec7a58e711db4e
Jeune Milford <milford.j@maristcollege.school.nz>
2022-07-25 14:46:54 +1200

    found error did not change const to let

--- script.js
+++ script.js
@@ -1,29 +1,53 @@
 const StartButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById
 ('question-container')
-
-const shuffledQuestions, currentQuestionIndex
+const questionElement = document.getElementById('question')
+const answerButtonElement = document.getElementById('answer-buttons')
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
 console.log('Started')
 startButton.classlist.add('hide')
+shuffledQuestions = questions.sort(() => Math.random() - .5)
+currentQuestionIndex = 0
 questionContainerElement.classList.remove('hide')
 setNextQuestion()
 }
 
 function setNextQuestion() {
+resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 
 }
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer =>{
+    const button = document.createElement('button')
+    button.innerText = answer.Text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonElement.appendChild(button)
+  })
+}
+
+function resetState() {}
+  nextButon.classList.add('hide')
+  while (answerButtonElement.FirstChild) 
+  answerButtonElement.removeChild
+  (answerButtonElement.firstChild)
 
 function selectAnswer() {
-
+  showQuestion(shuffledQuestions)[currentQuestionIndex] 
 }
 
 const question = [
   {
-    question:'What is 2 + 2',
+    question:'What is 2 + 2?',
     answers: [
         { Text:'4', correct: true},
         {Text:'22', correct:false}
